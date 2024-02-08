2f92314a8f85228a51752c7185fe123f23500c2d
Zakaria-morjani <zakaria.morjani01@gmail.com>
2024-02-08 14:23:07 -0500

    exercice dragon contre perso

--- PO1_Encapsulation/Form1.cs
+++ PO1_Encapsulation/Form1.cs
@@ -2,24 +2,58 @@ namespace PO1_Encapsulation
 {
     public partial class Form1 : Form
     {
+        Personnage joueur = new Personnage(); // je me cree un personnage
+        Personnage dragon = new();
+
         public Form1()
         {
             InitializeComponent();
-        }
+            //propriete pour acceeder aux attributs privé. on selectionne nos attribut dans la classe puis on fait action rapide et apres encapsuler les champs et utuliser la propriété
+            joueur.PointsDeVie = 10;
+            dragon.PointsDeVie = joueur.PointsDeVie * 3;
+            joueur.PointsDePuissance = 1;
+            dragon.PointsDePuissance = joueur.PointsDePuissance * 3;
 
+
+        }
+        private void ActualiserLabel()
+        {
+            lbl_pointsVieJoueur.Text = "point de vie :" + joueur.PointsDeVie.ToString(); //pour changer le label et mettre devant les pdv du joueur 
+
+            lbl_pointsVieDragon.Text = "point de vie: " + dragon.PointsDeVie.ToString(); //pour changer le label et mettre devant les pdv du dragon 
+
+            lbl_puissanceAttaqueJoueur.Text = " puissance d'attaque: " + joueur.PointsDePuissance.ToString();
+
+            lbl_puissanceAttaqueDragon.Text = "puissance d'attaque: " + dragon.PointsDePuissance.ToString();
+        }
         private void btn_attaquer_Click(object sender, EventArgs e)
         {
-
+            ActualiserLabel();
+            TourDragon();
+            dragon.PointsDeVie -= joueur.PointsDePuissance;  // si le joueur tape le dragon il s enleve 1 de vie 
         }
 
         private void btn_seSoigner_Click(object sender, EventArgs e)
         {
-
+            joueur.PointsDeVie += 5;
+            TourDragon();
+            ActualiserLabel();
         }
 
         private void btn_puissanceAttaque_Click(object sender, EventArgs e)
         {
+            joueur.PointsDePuissance += 1;
+            TourDragon();
+            ActualiserLabel();
+        }
 
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ActualiserLabel();
+        }
+        private void TourDragon()
+        {
+            joueur.PointsDeVie -= dragon.PointsDePuissance;
         }
     }
 }
---
+++ PO1_Encapsulation/Personnage.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PO1_Encapsulation
+{
+    public class Personnage
+    {
+        //creer l attribut 
+        private string nom;
+        private int pointsDeVie;
+        private int pointsDePuissance;
+
+        public string Nom { get => nom; set => nom = value; }
+        public int PointsDeVie             // on met une regle ici pour faire en sorte que des chiffres ou qlq chose peut pas rentrer
+        {
+            get => pointsDeVie;
+            set
+            {
+                if (value > 0)
+                {
+                    pointsDeVie = value;
+                }
+                else
+                {
+                    pointsDeVie = 0;
+                }
+            }
+        }
+        public int PointsDePuissance { get => pointsDePuissance; set => pointsDePuissance = value; }
+
+        public bool EstEnVie  //pour savoir si le joueur est en vie 
+        {
+            get
+            {
+                if (pointsDeVie > 0)
+                {
+                    return true;
+                }
+                return false;
+                
+            }
+        }
+
+    }
+}
